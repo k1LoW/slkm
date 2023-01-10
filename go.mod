75363643b57a69c1a0ee4cc98f424774c6814353
k1LoW <k1lowxb@gmail.com>
2023-01-10 14:59:45 +0900

    Add test

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/k1LoW/slkm
 
 go 1.19
 
-require github.com/slack-go/slack v0.12.1
+require (
+	github.com/google/go-cmp v0.5.7
+	github.com/slack-go/slack v0.12.1
+)
 
 require github.com/gorilla/websocket v1.4.2 // indirect
--- slkm_test.go
+++ slkm_test.go
@@ -2,10 +2,51 @@ package slkm
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"os"
 	"regexp"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/slack-go/slack"
 )
+
+func TestReplaceBlockMentions(t *testing.T) {
+	if os.Getenv("SLACK_API_TOKEN") == "" {
+		t.Skip("not set SLACK_API_TOKEN")
+	}
+	tests := []struct {
+		in slack.Block
+	}{
+		{slack.NewSectionBlock(slack.NewTextBlockObject("mrkdwn", "hello @here", false, false), nil, nil)},
+		{slack.NewSectionBlock(nil, []*slack.TextBlockObject{slack.NewTextBlockObject("mrkdwn", "hello @k1low", false, false)}, nil)},
+	}
+	ctx := context.Background()
+	c, err := New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			a, err := json.Marshal(tt.in)
+			if err != nil {
+				t.Error(err)
+			}
+			if err := c.replaceBlockMentions(ctx, tt.in); err != nil {
+				t.Error(err)
+			}
+			b, err := json.Marshal(tt.in)
+			if err != nil {
+				t.Error(err)
+			}
+			if diff := cmp.Diff(a, b, nil); diff == "" {
+				t.Error("want diff")
+			}
+		})
+	}
+
+}
 
 func TestReplaceMentions(t *testing.T) {
 	if os.Getenv("SLACK_API_TOKEN") == "" {
